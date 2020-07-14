ed6caf1da44315e0888f24796826228391a80715
Viv <hvivian2012@yahoo.com>
2020-07-13 23:11:44 -0700

    Title

---
+++ myScript.js
@@ -0,0 +1,16 @@
+// window.onscroll = function() {myFunction()};
+//
+// // Get the navbar
+// var centeredband = document.getElementById("navbar");
+//
+// // Get the offset position of the navbar
+// var sticky = centeredband.offsetTop;
+//
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     centeredband.classList.add("sticky")
+//   } else {
+//     centeredband.classList.remove("sticky");
+//   }
+// }
